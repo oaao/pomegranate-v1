c7e52003d994cafa2237d0823fde68956bd9d8a0
Svara Eir <svara.eir@gmail.com>
2016-06-10 17:05:46 -0400

    partial market history implementation, broke off .csv-related code - implemented concurrent async lookups of market history for each typeID (but forgets its own typeID...) - removed all code pertinent to .csv reading/writing and data structures thereof, as displaying the Name of a given typeID is pointless until the end

--- _CURRENT.py
+++ _CURRENT.py
@@ -4,11 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 from requests_futures.sessions import FuturesSession
 import arrow
 import json
-import csv
 from os import path
 
 c_str = lambda x: "{:,}".format(x)
-base_dir = path.dirname(path.realpath('__file__'))
 
 
 def market_configs(hub_spec):
@@ -23,9 +21,13 @@ def market_configs(hub_spec):
     return hub_choice
 
 
-def url_format(region_spec):
-    url_base = 'https://crest-tq.eveonline.com/market/' + region_spec + '/orders/all/'
-    return url_base
+def url_format(region_id, req_type):
+    url_parent = 'https://crest-tq.eveonline.com/market/'
+    if req_type is 'orders':
+        url_result = url_parent + region_id + '/orders/all/'
+    elif req_type is 'context':
+        url_result = url_parent + region_id + '/types/'
+    return url_result
 
 
 def url_async(url_list, worker_limit):
@@ -47,18 +49,11 @@ def group_dictvalue(raw_list, dictvalue):
     return new_dict
 
 
-def csv_makedict(f_dir, f_name, k, v, enc):
-    with open(path.join(f_dir, f_name), mode='r', encoding=enc) as csv_input:
-        csv_read = csv.reader(csv_input)
-        csv_dict = {rows[k]: rows[v] for rows in csv_read}
-        return csv_dict
-
-
 def market_import(hub_spec):
     t_import = time.time()
     data_configs = market_configs(hub_spec)
     hub_regionid = str(data_configs[0])
-    url_base = url_format(hub_regionid)
+    url_base = url_format(hub_regionid, 'orders')
     url_market = url_base + '?page='
     print('\nMARKET_IMPORT: Trying: %s' % url_base)
     data_pages = requests.get(url_base).json()['pageCount']
@@ -71,11 +66,11 @@ def market_import(hub_spec):
     return data_items, data_configs
 
 
-def market_distill(raw_list, configs):
+def market_distill(raw_list, config):
     t_hub = time.time()
     print('MARKET_DISTILL: Purging all non-hub orders.')
     data_total = raw_list
-    hub_stationid = configs[1]
+    hub_stationid = config[1]
     data_hubonly = [x for x in data_total if hub_stationid == x['stationID']]
     print('MARKET_DISTILL: %s entries preserved; %s entries purged.'
           % (c_str(len(data_hubonly)), c_str(len(raw_list) - len(data_hubonly))))
@@ -121,15 +116,23 @@ def market_distill(raw_list, configs):
     return data_grouped_station
 
 
-def market_context(raw_list):
-    context_namelist = csv_makedict('resources', 'invTypes_small.csv', 0, 2, 'utf-8')
-    # take out the station value from each
+def market_context(raw_list, config):
+    t_history = time.time()
+    data_distilled = raw_list
+    hub_regionid = str(config[0])
+    type_ids = data_distilled.keys()
+    print('MARKET_CONTEXT: Acquiring market history for %s typeIDs.'
+          % c_str(len(type_ids)))
+    url_context = url_format(hub_regionid, 'context')
+    url_set = [url_context + str(x) + '/history/' for x in type_ids]
+    data_res = url_async(url_set, len(type_ids))
+    print("--- %s seconds ---\n" % (time.time() - t_history))
+    # need to restructure this so the typeID is preserved
+    # data_context = [x for i in data_res for x in i['items']]
     # make a new list that contains:
-    # - name that corresponds w/ ID
-    # - station id
     # - pricing/volume info
     # add to dict such that {'typeid': [[orders],[context]]
-    data_contextualised = ''
+    data_contextualised = {}
     return data_contextualised
 
 
@@ -149,7 +152,7 @@ t0 = time.time()
 
 orders_raw, orders_config = market_import('rens')
 orders_structured = market_distill(orders_raw, orders_config)
-orders_contextualised = market_context(orders_structured)
+orders_contextualised = market_context(orders_structured, orders_config)
 
 write_json(orders_structured)
 
